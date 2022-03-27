f1c171ccac538a8855e1eab4ee4a8d9adfd7e8ed
帅哥别 <626520985@qq.com>
2022-03-27 23:15:39 +0800

    feat: add factorialCache in factorial.test.js

--- factorial.test.js
+++ factorial.test.js
@@ -6,8 +6,20 @@
 function factorial(n) {
   return n <= 1 ? 1 : n * factorial(n - 1);
 }
+function factorialCache(n) {
+  const cache = [1, 2];
+  for (let i = 2; i < n + 1; i++) {
+    cache.push((i + 1) * cache[i - 1]);
+  }
+  return cache[n - 1];
+}
 
 test('factorial', () => {
   const n = 10;
   expect(factorial(n)).toEqual(3628800);
+});
+
+test('factorial cache', () => {
+  const n = 11;
+  expect(factorialCache(n)).toEqual(3628800 * 11);
 });
