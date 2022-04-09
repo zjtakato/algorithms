90ee18d296f5e407e66ad999ea30ddce05713327
帅哥别 <626520985@qq.com>
2022-04-09 15:53:25 +0800

    feat: add binary-search.test.js file

---
+++ binary-search.test.js
@@ -0,0 +1,41 @@
+function binarySearch(id, array) {
+  let min = 0;
+  let max = array.length - 1;
+  let index;
+  let element;
+
+  while (min <= max) {
+    index = Math.floor((min + max) / 2);
+    element = array[index];
+
+    if (element.id < id) {
+      min = index + 1;
+    } else if (element.id > id) {
+      max = index - 1;
+    } else {
+      return element;
+    }
+  }
+  return void 0;
+}
+test("binary search", function () {
+  const lookingFor = { id: 23, name: "Brian" };
+  expect(
+    binarySearch(23, [
+      { id: 1, name: "Sam" },
+      { id: 3, name: "Sarah" },
+      { id: 5, name: "John" },
+      { id: 6, name: "Burke" },
+      { id: 10, name: "Simona" },
+      { id: 12, name: "Asim" },
+      { id: 13, name: "Niki" },
+      { id: 15, name: "Aysegul" },
+      { id: 17, name: "Kyle" },
+      { id: 18, name: "Jem" },
+      { id: 19, name: "Marc" },
+      { id: 21, name: "Chris" },
+      lookingFor,
+      { id: 24, name: "Ben" }
+    ])
+  ).toBe(lookingFor);
+});
