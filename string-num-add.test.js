6f3b62f8483ba467142c2ac8056c15ad6aaf0349
zjtakato <zjtakato@gmail.com>
2023-03-07 22:05:45 +0800

    fix: 再次优化string-num-add
    - 主要是为了消掉最后的数组反转和最开始的Array.from
    - 关于数组的操作，有可能也是一个O(n)的操作

--- string-num-add.test.js
+++ string-num-add.test.js
@@ -1,30 +1,43 @@
 function add(a, b) {
-  let result = [];
-  a = Array.from(a);
-  b = Array.from(b);
-  const longArray = a.length >= b.length ? a : b;
-  const shortArray = a.length < b.length ? a : b;
-  let carry = 0;
-  while (shortArray.length) {
-    const addResult = parseInt(shortArray.pop()) + parseInt(longArray.pop()) + carry;
-    const remainder = addResult % 10;
-    carry = addResult >= 10 ? 1 : 0;
-    result.push(remainder);
+  if (a === '0') return b;
+  if (b === '0') return a;
+  let result = '';
+  let max = a.length >= b.length ? a : b;
+  let min = max === a ? b : a;
+  let step = 0;
+  let i = 0;
+  while (i !== min.length) {
+    const a1 = parseInt(max[max.length - i - 1]);
+    const b1 = parseInt(min[min.length - i - 1]);
+    const sum = a1 + b1 + step;
+    step = sum >= 10 ? 1 : 0;
+    const remainder = sum % 10;
+    result = remainder + result;
+    i++;
   }
-  while (longArray.length) {
-    if (carry === 0) break;
-    const addResult = parseInt(longArray.pop()) + carry;
-    const remainder = addResult % 10;
-    carry = addResult >= 10 ? 1 : 0;
-    result.push(remainder);
+  while (i !== max.length) {
+    if (step === 0) break;
+    const a1 = parseInt(max[max.length - i - 1]);
+    const sum = a1 + step;
+    step = sum >= 10 ? 1 : 0;
+    const remainder = sum % 10;
+    result = remainder + result;
+    i++;
   }
-  result = result.concat(longArray.reverse());
-  carry > 0 && result.push(carry);
-  return result.reverse().join('');
+  if (i !== max.length) {
+    result = max.slice(0, max.length - i) + result;
+  }
+  if (step) {
+    result = step + result;
+  }
+  return result;
 }
 
-test('add', () => {
-  expect(add('2', '1')).toEqual('3');
-  expect(add('99', '99')).toEqual('198');
-  expect(add('11199', '9')).toEqual('11208');
-});
+console.log(add('11199', '9'));
+
+// test('add', () => {
+//   expect(add('2', '1')).toEqual('3');
+//   expect(add('99', '99')).toEqual('198');
+//   expect(add('11199', '9')).toEqual('11208');
+
+// });
